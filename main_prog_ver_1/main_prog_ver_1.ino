cad75dbfce2ef1bac61896ccf72903569b75c793
Tyrell2018 <tyrell2018@mail.ugm.ac.id>
2019-02-22 00:12:40 +0700

    Malam_Jum'at

--- main_prog_ver_1/main_prog_ver_1.ino
+++ main_prog_ver_1/main_prog_ver_1.ino
@@ -5,7 +5,7 @@
 
 //Define global variables, save the bluetooth
 int input;
-int buttonState[6];
+bool buttonState[5];
 bool Status = 0;
 
 void setup() {
@@ -18,6 +18,8 @@ void setup() {
   digitalWrite(directionPin_M1, LOW);
   digitalWrite(speedPin_M2, 0);
   digitalWrite(directionPin_M2, LOW);
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -29,7 +31,7 @@ void loop() {
   //controll motor 1
   void motor_M1(int speed){
     if(speed == 0){
-      analogWrite(directionPin_M1, 0); //speed = 0, stop
+      analogWrite(directionPin_M1, speed); //speed = 0, stop
     }
     if(speed > 0){
       digitalWrite(directionPin_M1, HIGH);//move forward
@@ -43,7 +45,7 @@ void loop() {
   //controll motor 2
   void motor_M2(int speed){
     if(speed == 0){
-      analogWrite(directionPin_M2, 0); //speed = 0, stop
+      analogWrite(directionPin_M2, speed); //speed = 0, stop
     }
     if(speed > 0){
       digitalWrite(directionPin_M2, HIGH);//move forward
@@ -64,33 +66,64 @@ void loop() {
   void receive_BLE(){
     if(Serial.available()){
       input = Serial.read();
+      Status = 1;
       switch(input){
-        Status=1;
-        case '0': buttonState[0] = input; break; 
-        case '1': buttonState[1] = input; break; 
-        case '2': buttonState[2] = input; break; 
-        case '3': buttonState[3] = input; break; 
-//        case '4': Status = 1; break;
-//        case '5': Status = 0; break;
+        case '0': buttonState[0] = true; 
+        buttonState[1] = false;
+        buttonState[2] = false;
+        buttonState[3] = false;
+        buttonState[4] = false;break;
+        case '1': buttonState[1] = true; 
+        buttonState[0] = false;
+        buttonState[2] = false;
+        buttonState[3] = false;
+        buttonState[4] = false;break;
+        case '2': buttonState[2] = true; 
+        buttonState[0] = false;
+        buttonState[1] = false;
+        buttonState[3] = false;
+        buttonState[4] = false;break; 
+        case '3': buttonState[3] = true; 
+        buttonState[0] = false;
+        buttonState[1] = false;
+        buttonState[2] = false;
+        buttonState[4] = false;break; 
+        case '4': buttonState[4] = true; 
+        buttonState[0] = false;
+        buttonState[1] = false;
+        buttonState[2] = false;
+        buttonState[3] = false;break; 
       }
-    }
+
+      /*if(input == 4){
+        Status = 1;
+      } else if(input == 5){
+        Status = 0;
+      } */
+    } 
   }
 
   //bluetooth mode
   void BLEMode(){
-    if(buttonState[0] == 0){
+    
+    if(buttonState[1] == true){
       motorControll(150, 150); //move forward
+      Serial.println("Maju");
     }
-    else if(buttonState[1] == 1){
+    else if(buttonState[2] == true){
       motorControll(-150, -150); //move backward
+      Serial.println("Mundur");
     }
-    else if(buttonState[2] == 2){
-      motorControll(-200, 200); //move right
+    else if(buttonState[3] == true){
+      motorControll(200, -200); //move right
+      Serial.println("Kanan");
     }
-    else if(buttonState[3] == 3){
-      motorControll(200, -200); //move left
+    else if(buttonState[4] == true){
+      motorControll(-200, 200); //move left
+      Serial.println("Kiri");
     }
-    else {
-      motorControll(0, 0); //stop
+    else if(buttonState[0] == true){
+      motorControll(255, 255); //stop
+      Serial.println("Stop");
     }
   }
